06cb4ddfdee9ebdd21be92614cba9bc604e79984
Jeff Bloom <bloo0178@gmail.com>
2019-05-29 13:57:18 -0400

    Updated favicon

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import styles from "./App.module.css";
-import Map from './Map.jsx';
+import Map from "./Map.jsx";
 
 class App extends Component {
-
-
   render() {
-
     return (
       <React.Fragment>
-      <h1>U.S. Educational Attainment</h1>
-      <h3>Percentage of adults ages 25 and older with a bachelor's degree or higher (2010-2014)</h3>
-      <div className={styles.App}>
-        <Map />
-      </div>
+        <h1>U.S. Educational Attainment</h1>
+        <h3>
+          Percentage of adults ages 25 and older with a bachelor's degree or
+          higher (2010-2014)
+        </h3>
+        <div className={styles.App}>
+          <Map />
+        </div>
       </React.Fragment>
     );
   }
